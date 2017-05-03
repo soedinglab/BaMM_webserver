1f13a988924d59da550ed2fae0cdd0724d304e44
bammmotif administrator <bammmotif_admin@s1161-marvin.mpibpc.intern>
2017-05-03 15:34:38 +0200

    update amail settings

--- webserver/settings.py
+++ webserver/settings.py
@@ -185,7 +185,7 @@ EMAIL_HOST_USER = 'akiesel1@mpibpc.mpg.de'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 SERVER_EMAIL = 'akiesel1@mpibpc.mpg.de'
-DEFAULT_FROM_EMAIL = 'BaMMmotif'
+DEFAULT_FROM_EMAIL = 'akiesel1@mpibpc.mpg.de'
 
 #EMAIL_HOST = 'smtp.gmail.com'
 #EMAIL_HOST_PASSWORD = 'superserver'
