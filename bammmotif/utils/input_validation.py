cf790da85e386cb17ca6f21a401e29aab9b8bdf4
Christian Roth <christian.roth@mpibpc.mpg.de>
2017-11-02 15:15:24 +0100

    remodelled input file validation

---
+++ bammmotif/utils/input_validation.py
@@ -0,0 +1,57 @@
+import subprocess
+
+FASTA_VALIDATOR_BINARY = 'validate_fasta_file'
+BINDINGSITE_VALIDATOR_BINARY = 'validate_bindingsite_file'
+
+
+class ServerValidationError:
+    pass
+
+
+def validate_fasta_file(file_path):
+
+    result_description = {
+        0: 'Valid fasta file',
+        2: 'Cannot read fasta file',
+        3: 'Invalid fasta format',
+    }
+
+    command = [
+        FASTA_VALIDATOR_BINARY,
+        '%r' % file_path
+    ]
+    process = subprocess.run([str(x) for x in command])
+    exit_code = process.return_code
+
+    if exit_code < 0:
+        raise ServerValidationError('received signal %s' % -exit_code)
+    elif exit_code not in result_description:
+        raise ServerValidationError('unexpected exit code %s' % exit_code)
+
+    success = exit_code == 0
+    return success, result_description[exit_code]
+
+
+def validate_bindingsite_file(file_path, file_format):
+
+    result_description = {
+        0: 'All good.',
+        2: 'Cannot open file',
+        3: 'Invalid file format',
+    }
+
+    command = [
+        BINDINGSITE_VALIDATOR_BINARY,
+        '%r' % file_path,
+        file_format
+    ]
+    process = subprocess.run([str(x) for x in command])
+    exit_code = process.return_code
+
+    if exit_code < 0:
+        raise ServerValidationError('received signal %s' % -exit_code)
+    elif exit_code not in result_description:
+        raise ServerValidationError('unexpected exit code %s' % exit_code)
+
+    success = exit_code == 0
+    return success, result_description[exit_code]
