379b681aab83782b35e4a2cfcb865aa1f05fc25c
anjakiesel <kiesel@genzentrum.lmu.de>
2017-11-02 16:16:22 +0100

    update population scripts

--- populate.sh
+++ populate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 python manage.py populate_encode DB/ DB/ENCODE.hg19.TFBS.QC.metadata.jun2012-TFs_SPP_pooled.tsv
-python manage.py populate_example
+# python manage.py populate_example
