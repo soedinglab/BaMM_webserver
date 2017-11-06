9cdc761c6b4f160d03e2d33d8d6f30b57629cd6a
anjakiesel <kiesel@genzentrum.lmu.de>
2017-11-06 09:20:45 +0100

    Warning when wrong job ID is given

--- bammmotif/utils/misc.py
+++ bammmotif/utils/misc.py
@@ -14,7 +14,7 @@ import datetime
 import traceback
 import subprocess
 from shutil import copyfile
-
+import re
 
 def get_result_folder(job_id):
     return path.join(settings.JOB_DIR_PREFIX, str(job_id), 'Output')
@@ -254,3 +254,10 @@ def transfer_motif(job_pk):
         copyfile(src, dest)
         offs = 2
     return offs
+
+
+def valid_uuid(uuid):
+    regex = re.compile('^[a-f0-9]{8}-?[a-f0-9]{4}-?4[a-f0-9]{3}-?[89ab][a-f0-9]{3}-?[a-f0-9]{12}\Z', re.I)
+    match = regex.match(uuid)
+    return bool(match)
+    
--- bammmotif/views.py
+++ bammmotif/views.py
@@ -19,7 +19,7 @@ from .utils import (
     get_log_file,
     get_user, set_job_name, upload_example_fasta,
     upload_example_motif, get_result_folder,
-    upload_db_input
+    upload_db_input,valid_uuid
 )
 import datetime
 import os
@@ -197,10 +197,15 @@ def find_results(request):
         form = FindForm(request.POST)
         if form.is_valid():
             jobid = form.cleaned_data['job_ID']
-            return redirect('result_detail', pk=jobid)
+            if valid_uuid(jobid):
+                job = Job.objects.get(pk=jobid).exists()
+                if job.exists():
+                    return redirect('result_detail', pk=jobid)
+            form = FindForm()
+            return render(request, 'results/results_main.html', {'form': form, 'warning': True})                
     else:
         form = FindForm()
-    return render(request, 'results/results_main.html', {'form': form})
+    return render(request, 'results/results_main.html', {'form': form, 'warning': False})
 
 
 def result_overview(request):
