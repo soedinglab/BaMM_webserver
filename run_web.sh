0fae999b278eef19f5136916665501c8fe57cc88
mwess <maxwess@googlemail.com>
2018-01-16 17:50:05 +0100

    updated run_web.sh so that commenting in and out isn't necessary anymore

--- run_web.sh
+++ run_web.sh
@@ -6,7 +6,11 @@ python manage.py makemigrations
 python manage.py migrate
 
 # fill db construct (only use this when setting up webserver on completely new environment!)
-./populate.sh
+if [ -f ! POPULATED ]
+then
+    ./populate.sh
+    touch POPULATED
+fi
 
 # start webserver
 python manage.py runserver 0.0.0.0:10080 
