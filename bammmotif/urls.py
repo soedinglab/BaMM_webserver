f9ad3171234124669da053d375aa067aae2c19dc
mwess <maxwess@googlemail.com>
2018-01-18 15:10:23 +0100

    direct urls to bamm_views

--- bammmotif/urls.py
+++ bammmotif/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 from . import views
 from bammmotif.peng import views as peng_views
+from bammmotif.bamm import views as bamm_views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
@@ -13,16 +14,24 @@ urlpatterns = [
     url(r'^contact/', views.contact, name='contact'),
     url(r'^imprint/', views.imprint, name='imprint'),
     url(r'^job/submitted/', views.submitted, name='submitted'),
-    url(r'^job/bamm/$', views.run_bamm_view, name='run_bamm'),
-    url(r'^job/bamm/(?P<mode>\w+)/$', views.run_bamm_view, name='run_bamm'),
+    # url(r'^job/bamm/$', views.run_bamm_view, name='run_bamm'),
+    # url(r'^job/bamm/(?P<mode>\w+)/$', views.run_bamm_view, name='run_bamm'),
+    url(r'^job/bamm/$', bamm_views.run_bamm_view, name='run_bamm'),
+    url(r'^job/bamm/(?P<mode>\w+)/$', bamm_views.run_bamm_view, name='run_bamm'),
     url(r'^job/bamm_scan/$', views.run_bammscan_view, name='run_bamm_scan'),
     url(r'^job/bamm_scan/(?P<mode>\w+)/$', views.run_bammscan_view, name='run_bamm_scan'),
     url(r'^job/bamm_scan/(?P<mode>\w+)/(?P<pk>.*)/$', views.run_bammscan_view, name='run_bamm_scan'),
     url(r'^job/bamm_compare/$', views.run_compare_view, name='run_compare'),
     url(r'^job/bamm_compare/(?P<mode>\w+)/$', views.run_compare_view, name='run_compare'),
-    url(r'^results/$', views.find_results, name='find_results'),
-    url(r'^results/result_overview/$', views.result_overview, name='result_overview'),
-    url(r'^results/(?P<pk>.*)/$', views.result_detail, name='result_detail'),
+    url(r'^results/$', bamm_views.find_results, name='find_results'),
+    url(r'^results/result_overview/$', bamm_views.result_overview, name='result_overview'),
+    url(r'^results/(?P<pk>.*)/$', bamm_views.result_detail, name='result_detail'),
+    #url(r'^results/$', views.find_results, name='find_results'),
+    #url(r'^results/result_overview/$', views.result_overview, name='result_overview'),
+    #url(r'^results/(?P<pk>.*)/$', views.result_detail, name='result_detail'),
+    #url(r'^results/$', views.find_results, name='find_results'),
+    #url(r'^results/result_overview/$', views.result_overview, name='result_overview'),
+    #url(r'^results/(?P<pk>.*)/$', views.result_detail, name='result_detail'),
     url(r'^delete/(?P<pk>.*)/$', views.delete, name='delete'),
     url(r'^database/$', views.maindb, name='maindb'),
     url(r'^database/(?P<pk>.*)/$', views.db_detail, name='db_detail'),
