eb7f3b3158d4a4e4c09319a3e1d5a75b680107bc
Christian Roth <christian.roth@mpibpc.mpg.de>
2018-04-02 21:32:20 +0200

    updated database input view

--- bammmotif/database/forms.py
+++ bammmotif/database/forms.py
@@ -1,8 +1,9 @@
 from django import forms
 
-from ..utils.ui import DBChoiceField
+from ..utils.ui import DBChoiceField, HELP_TEXTS
 
 
 class DBForm(forms.Form):
-    search_term = forms.CharField(max_length=255, required=False)
-    database = DBChoiceField()
+    search_term = forms.CharField(max_length=255, required=False,
+                                  help_text=HELP_TEXTS['search_term'])
+    database = DBChoiceField(help_text=HELP_TEXTS['motif_db'])
